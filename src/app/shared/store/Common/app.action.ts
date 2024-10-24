ba5ab837c316998fc0781d5abbaa640056b96dea
iScarcy <56608800+iScarcy@users.noreply.github.com>
2024-10-24 15:28:58 +0200

    ho eliminato un action che non c'è

--- src/app/shared/store/Common/app.action.ts
+++ src/app/shared/store/Common/app.action.ts
@@ -2,9 +2,8 @@ import { createAction, props } from "@ngrx/store";
 
 export const SHOW_ALERT = '[app] show alert';
 export const EMPTY_ACTION = '[app] empty';
-export const LOADING = '[app] open loading'
-//export const LOAD_DATA_SUCCESS = '[app] load data success'
 
-export const loading        = createAction(LOADING)
+
+
 export const showalert      = createAction(SHOW_ALERT, props<{message: string, resulttype:string}>())
 export const emptyaction    = createAction(EMPTY_ACTION)
